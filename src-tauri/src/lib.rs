125c1d5980d49f21ba2e8a6dfd07cf71e2d5ff9a
Jan Decavele <jan@opcafegaan.be>
2024-10-04 18:39:56 +0200

    stash

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,14 +1,55 @@
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+pub mod models;
+pub mod schema;
+
+use diesel::prelude::*;
+use dotenvy::dotenv;
+use std::env;
+use crate::models::{NewPost, Post};
+pub fn establish_connection() -> SqliteConnection {
+    dotenv().ok();
+
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    SqliteConnection::establish(&database_url)
+        .unwrap_or_else(|_| panic!("Error connecting to {}", database_url))
 }
+#[tauri::command]
+fn get_posts() -> Vec<Post> {
+    println!("get_posts");
+    use self::schema::posts::dsl::*;
+
+    let connection = &mut establish_connection();
+    let results = posts
+        .filter(published.eq(true))
+        .limit(5)
+        .select(Post::as_select())
+        .load(connection)
+        .expect("Error loading posts");
+    results.into()
+}
+
+#[tauri::command]
+fn create_post(title: &str, body: &str) -> Post {
+    println!("create_post");
+    use crate::schema::posts;
+
+    let connection = &mut establish_connection();
+    let new_post = NewPost { title, body };
+
+    let inserted_post = diesel::insert_into(posts::table)
+        .values(&new_post)
+        .returning(Post::as_returning())
+        .get_result(connection)
+        .expect("Error saving new post");
+
+    inserted_post.into()
+}
+
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_shell::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![get_posts,create_post])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,71 @@
-import {useState} from "react";
 import {invoke} from "@tauri-apps/api/core";
-import "./App.css";
+import {
+    Box, Button,
+    ChakraProvider, Container,
+    FormControl,
+    Heading,
+    IconButton, Input,
+    StackDivider,
+    Text, Textarea,
+    VStack
+} from "@chakra-ui/react";
+import {ChangeEvent, useState} from "react";
+import {EmailIcon} from "@chakra-ui/icons";
 
 function App() {
-    const [greetMsg, setGreetMsg] = useState("");
-    const [name, setName] = useState("");
 
-    async function greet() {
-        // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-        setGreetMsg(await invoke("greet", {name: name.repeat(name.length)}));
+    const [posts, setPosts] = useState([]);
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+
+    const postMessage = async () => {
+        try {
+            const response = await invoke("create_post", {title, body});
+            debug(response);
+            await fetchPosts();
+        } catch (e) {
+            alert(`Error: ${e}`)
+        } finally {
+            setBody('');
+            setTitle('')
+        }
+    }
+    const fetchPosts = async () => {
+        await invoke("get_posts");
+        alert('response')
     }
 
     return (
-        <div className="container">
-            <h1>Welcome to Tauri!</h1>
-            <p>Click on the Tauri, Vite, and React logos to learn more.</p>
+        <ChakraProvider>
+            <Container>
+                <VStack divider={<StackDivider borderColor={'gray.200'}/>} spacing={4} align={'stretch'} padding={4}>
 
-            <form
-                className="row"
-                onSubmit={(e) => {
-                    e.preventDefault();
-                    greet();
-                }}
-            >
-                <input
-                    id="greet-input"
-                    onChange={(e) => setName(e.currentTarget.value)}
-                    placeholder="Enter a name..."
-                />
-                <button type="submit">Greet</button>
-            </form>
+                    {posts.map((post: any) => {
+                        return <Box h={'40px'} shadow={'md'}>
+                            <Heading>{post.title}</Heading>
+                            <Text>{post.body}</Text>
+                        </Box>
+                    })}
+                </VStack>
 
-            <p>{greetMsg}</p>
-        </div>
+
+                <form onSubmit={(event: SubmitEvent) => {
+                    event.preventDefault();
+                    alert(`submit: ${title} ${body}`);
+                    postMessage()
+
+                }}>
+                    <FormControl>
+                        <Input type="text" name="message" id="message"
+                               onChange={(event: ChangeEvent) => setTitle(event.currentTarget.value)}/>
+                        <Textarea name="body" placeholder='Here is a sample placeholder'
+                                  onChange={(event: ChangeEvent) => setBody(event.currentTarget.value)}/>
+                    </FormControl>
+                    <IconButton aria-label='Send message' name="submit" value={'plop'} type={'submit'}
+                                icon={<EmailIcon/>}/>
+                </form>
+            </Container>
+        </ChakraProvider>
     );
 }
 
