9865693e9b18a7eb56e0d47253078031e918eec0
Jan Decavele <jan.decavele@gmail.com>
2024-11-14 13:18:31 +0100

    Add deletion button

--- src-tauri/src/migrations.rs
+++ src-tauri/src/migrations.rs
@@ -8,6 +8,11 @@ pub fn get_migrations() -> Vec<Migration> {
             description: "create_initial_tables",
             sql: "CREATE TABLE posts (id INTEGER PRIMARY KEY, title TEXT, body TEXT);",
             kind: MigrationKind::Up,
+        }, Migration {
+            version: 2,
+            description: "Add timestamp to posts table",
+            sql: "ALTER TABLE posts ADD COLUMN created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP;",
+            kind: MigrationKind::Up,
         }
     ]
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import {
+    Badge,
     Box,
     ChakraProvider, Container,
     FormControl,
@@ -9,13 +10,14 @@ import {
     VStack
 } from "@chakra-ui/react";
 import {ChangeEvent, useState} from "react";
-import {EmailIcon} from "@chakra-ui/icons";
+import {DeleteIcon, EmailIcon} from "@chakra-ui/icons";
 import Database from "@tauri-apps/plugin-sql";
 
 interface Post {
     id: number,
     title: string,
     body: string
+    created_at: string
 }
 
 function App() {
@@ -25,14 +27,21 @@ function App() {
 
     const postMessage = async () => {
         const db = await Database.load('sqlite:mydatabase.db');
-        const response = await db.execute('INSERT INTO posts (title, body) VALUES (?, ?)', [title, body]);
+        await db.execute('INSERT INTO posts (title, body) VALUES (?, ?)', [title, body]);
+        setBody('')
+        setTitle('')
+        await getPosts()
+    }
 
+    const deleteMessage = async (id: number) => {
+        const db = await Database.load('sqlite:mydatabase.db');
+        await db.execute('DELETE FROM posts WHERE id = ?', [id]);
         await getPosts()
     }
 
     const getPosts = async () => {
         const db = await Database.load('sqlite:mydatabase.db');
-        const result = await db.select<Post[]>('SELECT id, title, body FROM posts');
+        const result = await db.select<Post[]>('SELECT id, title, body, created_at FROM posts');
 
         setPosts(result)
     }
@@ -42,11 +51,19 @@ function App() {
     return (
         <ChakraProvider>
             <Container>
+                <Heading size={"xl"}>
+                    Our messageboard:
+                </Heading>
                 <VStack divider={<StackDivider borderColor={'gray.200'}/>} spacing={4} align={'stretch'} padding={4}>
-
                     {posts.map((post: any) => {
-                        return <Box key={post.id} h={'40px'} shadow={'md'}>
-                            <Heading>{post.title}</Heading>
+                        return <Box key={post.id} padding={"4px"}>
+                            <Heading>{post.title}
+                                <Badge>{post.created_at}</Badge>
+                                <IconButton aria-label='Send message' name="delete" type={'button'}
+                                            onClick={() => deleteMessage(post.id)}
+                                            icon={<DeleteIcon/>}
+                                            padding={"4px"}/>
+                            </Heading>
                             <Text>{post.body}</Text>
                         </Box>
                     })}
@@ -54,15 +71,22 @@ function App() {
 
 
                 <form>
-                    <FormControl>
+                    <FormControl border={"dashed"} padding={"4px"}>
+                        <Heading size={"lg"}>
+                            Add a new message:
+                        </Heading>
                         <Input type="text" name="message" id="message"
-                               onChange={(event: ChangeEvent<HTMLInputElement>) => setTitle(event.currentTarget.value)}/>
+                               placeholder={'Enter your message here'}
+                               value={title}
+                               onChange={(event: ChangeEvent<HTMLInputElement>) => setTitle(event.currentTarget.value)}
+                        />
                         <Textarea name="body" placeholder='Here is a sample placeholder'
+                                  value={body}
                                   onChange={(event: ChangeEvent<HTMLTextAreaElement>) => setBody(event.currentTarget.value)}/>
+                        <IconButton aria-label='Send message' name="submit" value={'plop'} type={'button'}
+                                    onClick={() => postMessage()}
+                                    icon={<EmailIcon/>}/>
                     </FormControl>
-                    <IconButton aria-label='Send message' name="submit" value={'plop'} type={'button'}
-                                onClick={() => postMessage()}
-                                icon={<EmailIcon/>}/>
                 </form>
             </Container>
         </ChakraProvider>
