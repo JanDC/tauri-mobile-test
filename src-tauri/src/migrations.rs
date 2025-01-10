d4b74c2414fef4295fbb195b5284b20e3567cc30
Jan Decavele <jan.decavele@gmail.com>
2025-01-10 13:24:00 +0100

    Fixed seed migration

--- src-tauri/src/migrations.rs
+++ src-tauri/src/migrations.rs
@@ -1,19 +1,31 @@
 use tauri_plugin_sql::{Migration, MigrationKind};
 
+fn get_seed_list() -> Vec<Migration> {
+    vec![Migration {
+        version: 3,
+        description: "Seed database with lots of posts",
+        sql: "INSERT INTO posts (title, body) VALUES ('testtitle','testbody testbody testbody testbody testbody')",
+        kind: MigrationKind::Up,
+    }]
+}
+
 pub fn get_migrations() -> Vec<Migration> {
-    vec![
+    let mut migrations = vec![
         // Define your migrations here
         Migration {
             version: 1,
             description: "create_initial_tables",
             sql: "CREATE TABLE posts (id INTEGER PRIMARY KEY, title TEXT, body TEXT);",
             kind: MigrationKind::Up,
-        }, Migration {
+        },
+        Migration {
             version: 2,
             description: "Add timestamp to posts table",
-            sql: "ALTER TABLE posts ADD COLUMN created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP;",
+            sql: "ALTER TABLE posts ADD COLUMN created_at DATETIME;
+                 UPDATE posts SET created_at = datetime('now') WHERE created_at IS NULL;",
             kind: MigrationKind::Up,
-        }
-    ]
+        },
+    ];
+    migrations.extend(get_seed_list());
+    migrations
 }
-
